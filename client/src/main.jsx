7b3713319c1f0dda4ce376d96e24feb7b2f4dec9
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-19 17:15:08 +0530

    initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,8 @@ export const Context = createContext({ isAuthorized: false });
 
 const AppWrapper = () => {
   const [isAuthorized, setIsAuthorized] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
+
   return (
     <Context.Provider value={{ isAuthorized, setIsAuthorized, user, setUser }}>
       <App />
