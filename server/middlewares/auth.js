55aac787a2bdce0dc713787561249dda9d4e2c87
Rashmi Patil <hiitsrash24@gmail.com>
2024-09-20 10:41:47 +0530

    updated middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,15 +1,23 @@
-import {User} from '../modals/user.js';
-import { catchAsyncError } from './catchAsyncError.js';
-import ErrorHandler from './error.js';
-import jwt from 'jsonwebtoken';
+import { User } from "../modals/user.js";
+import { catchAsyncError } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
 
+export const isAuthorized = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("User Not Authorized. Please Login.", 401));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-export const isAuthorized = catchAsyncError(async(req, res, next)=>{
-    const {token} =req.cookies;
-    if(!token){
-        return next(new ErrorHandler("User Not Authorized", 400));
-    }
-    const decoded= jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.user=await User.findByID(decoded.id);
-    next();
+  if (!decoded || !decoded.id) {
+    return next(new ErrorHandler("Invalid or Expired Token.", 403));
+  }
+
+  req.user = await User.findByID(decoded.id);
+
+  if (!req.user) {
+    return next(new ErrorHandler("User Not Found.", 404));
+  }
+  next();
 });
--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,40 +1,48 @@
-// function ErrorHandler(message, statusCode){
-//     const error=new Error(message);
-//     error.statusCode = statusCode;
-//     return error;
-// }
-
 class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this, this.constructor);
   }
-  
-export const errorMiddleware = (err, req, res, next)=>{
-    err.message = err.message || "Internal server error";
-    err.statusCode=err.statusCode || 500;
-    if(err.name === "CastError"){
-        const message = `Resource not found. Invalid ${err.path}`;
-        err=new ErrorHandler(message, 400);
-    }
-    if(err.code===11000){
-        const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
-        err=new ErrorHandler(message, 400);
-    }
-    if(err.name==="JsonWebTokenError"){
-        const message = `JSON web token is invalid. Try Again!`;
-        err=new ErrorHandler(message, 400);
-    }
-    if(err.name==="TokenExpiredError"){
-        const message = `JSON Web Token is expired. Try Again!`;
-        err=new ErrorHandler(message, 400);
-    }
-    return res.status(err.statusCode).json(
-        {
-            success: false,
-            message: err.message,
-        }
-    );
+}
+
+export const errorMiddleware = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message || "Internal server error";
+  error.statusCode = err.statusCode || 500;
+
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
+  }
+  if (err.code === 11000) {
+    const duplicateField = Object.keys(err.keyValue);
+    const message = `Duplicate ${duplicateField} entered`;
+    error = new ErrorHandler(message, 400);
+  }
+  if (err.name === "JsonWebTokenError") {
+    const message = `JSON web token is invalid. Try Again!`;
+    error = new ErrorHandler(message, 401);
+  }
+  if (err.name === "TokenExpiredError") {
+    const message = `JSON Web Token is expired. Try Again!`;
+    err = new ErrorHandler(message, 401);
+  }
+  console.error(err);
+
+  if (process.env.NODE_ENV === "development") {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      stack: error.stack,
+    });
+  }
+
+  res.status(error.statusCode).json({
+    success: false,
+    message: error.message,
+  });
 };
 export default ErrorHandler;
